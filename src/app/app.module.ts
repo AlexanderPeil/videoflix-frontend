5d684190ce871d5d05bdd8083ab74e17ea845515
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-12-27 15:04:31 +0100

    created signup.comp, browse.comp, auth.service and video.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { BrowseComponent } from './components/browse/browse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    BrowseComponent
   ],
   imports: [
     BrowserModule,
